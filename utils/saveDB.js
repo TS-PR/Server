d8904e7388df9f78168002cf620d5763e8543678
wooseok123 <hosmimam@naver.com>
2023-06-09 16:06:21 +0900

    jaebale

--- utils/saveDB.js
+++ utils/saveDB.js
@@ -4,15 +4,19 @@ import crawler from "./crawler.js";
 
 const saveTldrData = async () => {
   try {
-    connectDB().then(async () => {
-      const tldrData = await crawler();
-      const promise = tldrData.map(async (el) => {
-        await TLDR.updateOne({ section: el.section }, { posts: el.textData });
+    connectDB()
+      .then(async () => {
+        const tldrData = await crawler();
+        const promise = tldrData.map(async (el) => {
+          await TLDR.updateOne({ section: el.section }, { posts: el.textData });
+        });
+
+        await Promise.all(promise);
+        disconnectDB();
+      })
+      .catch((err) => {
+        console.log(err);
       });
-      4;
-      await Promise.all(promise);
-      disconnectDB();
-    });
   } catch (err) {
     console.error(err);
     disconnectDB();
